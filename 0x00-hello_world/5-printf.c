8d1cadec090ac8c2f200635c2934c4cf731d20ad
wicpad <ndegwapurity6@gmail.com>
2023-02-20 15:41:20 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Use the function printf
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
