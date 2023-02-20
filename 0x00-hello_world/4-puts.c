d6e5469b6154d6deffd25f4ff1ed53f49186c969
wicpad <ndegwapurity6@gmail.com>
2023-02-20 15:37:52 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Program that Use the function puts
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
