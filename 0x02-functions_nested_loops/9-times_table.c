ff3c6e4abe2543f38e4b6e114d57aa18c7f0249e
wicpad <ndegwapurity6@gmail.com>
2023-02-22 09:53:49 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,27 +1,30 @@
-#include <stdio.h>
-/**
- * main - is entry of program
- *
- * Return: 0, if succesful
- */
-int main(void)
-{
-	/*
-	 * prints all single integers, with comma and space after
-	 * loops through 0-9, prints them,prints comma,print space,
-	 * use only putchar
-	 */
-	int x;
+#include "main.h"
 
-	for (x = 0 ; x < 10 ; x++)
+/**
+ * times_table - Prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
 	{
-		putchar((x % 10) + '0');
-		if (x == 9)
-		/*continue printing comma and space except if x == 9*/
-			continue;
-		putchar(',');
-		putchar(' ');
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
 	}
-	putchar('\n');
-	return (0);
 }
